2375d31bab0bd1a86310ec3b98529f3eff661848
Observer <87912493+O8server@users.noreply.github.com>
2021-09-26 22:54:05 +0300

    Got the sneak working.

--- src/debug/main.cpp
+++ src/debug/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     sf::Vector2u oldRootWinSize;
 
-    sf::Color background = sf::Color(0, 0, 0); // Window background color
+    sf::Color background = sf::Color(150, 220, 255); // Window background color
 
     sf::Font font;
 
@@ -37,10 +37,44 @@ int main()
     text.setCharacterSize(24); // in pixels, not points!
 
     // set the color
-    text.setFillColor(sf::Color::Red);
+    text.setFillColor(sf::Color::White);
 
     // Map
+    int mapWidth = 20;
+    int mapHeight = 20;
 
+    sf::Vector2f mapTileSize(24.f, 24.f);
+
+    sf::RectangleShape map[mapWidth][mapHeight];
+
+    for (int x = 0; x < mapWidth; x++)
+    {
+        for (int y = 0; y < mapHeight; y++)
+        {
+            map[x][y].setSize(mapTileSize);
+            map[x][y].setOutlineThickness(1.f);
+            map[x][y].setOutlineColor(sf::Color(0, 0, 0));
+            map[x][y].setPosition(sf::Vector2f(((rootWin.getSize().x / 2) - (mapWidth * mapTileSize.x / 2)) + (x * mapTileSize.x), (((rootWin.getSize().y / 2) - (mapHeight * mapTileSize.y / 2))) + (y * mapTileSize.y)));
+
+            //walls
+            map[0][y].setFillColor(sf::Color::Red);
+            map[x][0].setFillColor(sf::Color::Red);
+            map[mapWidth - 1][y].setFillColor(sf::Color::Red);
+            map[x][mapHeight - 1].setFillColor(sf::Color::Red);
+        }
+    }
+
+    int headPosX = 0;
+    int headPosY = 0;
+
+    //set walls
+    // for(int x = 0; x < mapWidth; x++)
+    // {
+    //     for(int y = 0; y < mapHeight; y++)
+    //     {
+    //         map[0][y].setFillColor(sf::Color::Red);
+    //     }
+    // }
 
     if (!font.loadFromFile("resources/arial.ttf"))
     {
@@ -57,22 +91,29 @@ int main()
     snakeTile.setTexture(texture);
     std::vector<sf::Sprite> snake = {snakeTile};
 
-    
-
     float snakeVelocity = 0;
 
     float snakeVelocityX = 0.f;
     float snakeVelocityY = 0.f;
 
-    for (int i = 0; i < snake.size(); i++)
-    {
-        snake.at(i).setPosition(sf::Vector2f((rootWin.getSize().x / 2) - (snake[i].getTexture()->getSize().x / 2), (rootWin.getSize().y / 2) - (snake[i].getTexture()->getSize().y / 2)));
-    }
+    // for (int i = 0; i < snake.size(); i++)
+    // {
+    //     //snake.at(i).setPosition(sf::Vector2f((rootWin.getSize().x / 2) - (snake[i].getTexture()->getSize().x / 2), (rootWin.getSize().y / 2) - (snake[i].getTexture()->getSize().y / 2)));
+    //     snake.at(i).setPosition(map[1][1].getPosition());
+    // }
+
+    snake.at(0).setPosition(map[1][1].getPosition());
 
     char myDir = 'N';
 
     while (rootWin.isOpen())
     {
+        headPosX = (snake.at(0).getPosition().x - map[0][0].getPosition().x) / mapTileSize.x;
+        headPosY = (snake.at(0).getPosition().y - map[0][0].getPosition().y) / mapTileSize.y;
+
+        // std::cout << "X: " << headPosX << "\n";
+        // std::cout << "Y: " << headPosY << "\n";
+
         // std::cout << texture.getSize().x;
         // std::cout << texture.getSize().y;
 
@@ -87,17 +128,22 @@ int main()
             snakeVelocity = snake[i].getTexture()->getSize().x;
         }
 
-        if(elapsed.asMilliseconds() >= 100)
-        {           
+        if (elapsed.asMilliseconds() >= 200)
+        {
+
             for (int i = 0; i < snake.size(); i++)
             {
-                snake.at(i).move(snakeVelocityX, snakeVelocityY);
+                if (i > 0)
+                {
+                    snake.at(snake.size() - i).setPosition(snake.at(snake.size() - (i + 1)).getPosition());
+                }
             }
 
+            snake.at(0).move(snakeVelocityX, snakeVelocityY);
             clock.restart();
         }
 
-        //snake.at(0).move(snakeVelocityX, snakeVelocityY);       
+        //snake.at(0).move(snakeVelocityX, snakeVelocityY);
 
         while (rootWin.pollEvent(event))
         {
@@ -151,41 +197,50 @@ int main()
 
                 case (sf::Keyboard::C):
                 {
-                    snake.at(0).setPosition(sf::Vector2f((rootWin.getSize().x / 2) - (snake[0].getTexture()->getSize().x / 2), (rootWin.getSize().y / 2) - (snake[0].getTexture()->getSize().y / 2)));
+                    snake.at(0).setPosition(map[1][1].getPosition());
+                    snakeVelocityX = 0;
+                    snakeVelocityY = 0;
                 }
                 break;
 
-                // case (sf::Keyboard::Space):
-                // {
-                //     snake[0].getTexture()->getSize().x += 1;
-                //     snake[0].getTexture()->getSize().y += 1;
+                case (sf::Keyboard::Space):
+                {
+                    // snake[0].getTexture()->getSize().x += 1;
+                    // snake[0].getTexture()->getSize().y += 1;
 
-                //     snake.at(0).setSize(squareSize);
-                // }
-                // break;
+                    // snake.at(0).setSize(squareSize);
+                    snakeVelocityX = 0;
+                    snakeVelocityY = 0;
+                }
+                break;
 
-                // case (sf::Keyboard::Backspace):
-                // {
-                //     if (snake[0].getTexture()->getSize().x > 0 || snake[0].getTexture()->getSize().y > 0)
-                //     {
-                //         snake[0].getTexture()->getSize().x -= 1;
-                //         snake[0].getTexture()->getSize().y -= 1;
-                //     }
+                    // case (sf::Keyboard::Backspace):
+                    // {
+                    //     if (snake[0].getTexture()->getSize().x > 0 || snake[0].getTexture()->getSize().y > 0)
+                    //     {
+                    //         snake[0].getTexture()->getSize().x -= 1;
+                    //         snake[0].getTexture()->getSize().y -= 1;
+                    //     }
 
-                //     snake.at(0).setSize(squareSize);
-                // }
-                // break;
+                    //     snake.at(0).setSize(squareSize);
+                    // }
+                    // break;
 
                 case (sf::Keyboard::Enter):
                 {
-                    snake.push_back(snakeTile);
+                    if ((elapsedInput.asMilliseconds() >= 200))
+                    {
+                        snake.push_back(snakeTile);
+                        snake.at(snake.size() - 1).setPosition(snake.at(0).getPosition());
+                        clock1.restart();
+                    }
                 }
                 break;
 
                 case (sf::Keyboard::Right):
                 {
                     //snake.at(0).setFillColor(sf::Color::Red);
-                    if ((snakeVelocityX != -snakeVelocity) && (elapsedInput.asMilliseconds() >= 101))// Check if snake.at(0) isn't goin the opposite direction so you don't eat yourself
+                    if ((snakeVelocityX != -snakeVelocity) && (elapsedInput.asMilliseconds() >= 101) && myDir != 'L') // Check if snake.at(0) isn't goin the opposite direction so you don't eat yourself
                     {
                         snakeVelocityX = snakeVelocity;
                         snakeVelocityY = 0.0f;
@@ -198,13 +253,12 @@ int main()
                 case (sf::Keyboard::Left):
                 {
                     //snake.at(0).setFillColor(sf::Color::Red);
-                    if (snakeVelocityX != snakeVelocity && (elapsedInput.asMilliseconds() >= 101))
+                    if (snakeVelocityX != snakeVelocity && (elapsedInput.asMilliseconds() >= 101) && myDir != 'R')
                     {
                         snakeVelocityX = -snakeVelocity;
                         snakeVelocityY = 0.0f;
                         myDir = 'L';
                         clock1.restart();
-
                     }
                 }
                 break;
@@ -212,13 +266,12 @@ int main()
                 case (sf::Keyboard::Up):
                 {
                     //snake.at(0).setFillColor(sf::Color::Red);
-                    if (snakeVelocityY != snakeVelocity && (elapsedInput.asMilliseconds() >= 101))
+                    if (snakeVelocityY != snakeVelocity && (elapsedInput.asMilliseconds() >= 101) && myDir != 'D')
                     {
                         snakeVelocityX = 0.0f;
                         snakeVelocityY = -snakeVelocity;
                         myDir = 'U';
                         clock1.restart();
-
                     }
                 }
                 break;
@@ -226,7 +279,7 @@ int main()
                 case (sf::Keyboard::Down):
                 {
                     //snake.at(0).setFillColor(sf::Color::Red);
-                    if (snakeVelocityY != -snakeVelocity && (elapsedInput.asMilliseconds() >= 101))
+                    if (snakeVelocityY != -snakeVelocity && (elapsedInput.asMilliseconds() >= 101) && myDir != 'U')
                     {
                         snakeVelocityX = 0.0f;
                         snakeVelocityY = snakeVelocity;
@@ -274,13 +327,22 @@ int main()
                 break;
             }
         }
-        
-    
+
         rootWin.clear(background);
+
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                rootWin.draw(map[i][j]);
+            }
+        }
+
         for (int i = 0; i < snake.size(); i++)
         {
             rootWin.draw(snake.at(i));
         }
+
         rootWin.draw(text);
         rootWin.display();
     }
